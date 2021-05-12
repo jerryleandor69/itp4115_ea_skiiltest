317bbb27cad60ac77c7a0446aa88f768867bf413
EC2 Default User <ec2-user@ip-172-31-4-134.ec2.internal>
2021-05-12 09:04:05 +0000

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+import logging
+from flask import Flask
+from flask_login import LoginManager
+from flask_appbuilder import AppBuilder, SQLA
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+appbuilder = AppBuilder(app, db.session)
+
+from app import routes, models
